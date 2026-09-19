0fce7ec0fdfb9c979fa568800bb382e552d6e6c6
alina18-code <alinasoomro201118@gmail.com>
2026-09-19 20:31:05 +0500

    work on getting weather data

--- app/weather.py
+++ app/weather.py
@@ -7,3 +7,42 @@ load_dotenv()
 api_key = os.getenv("WEATHER_API")
 
 print(f"API key loaded finally: {api_key is not None}")
+
+def get_info (lat, lon, API_Key, unit="matric"):
+    url = "https://api.openweathermap.org/data/3.0/onecall?"
+
+    parameters ={
+        "lat": lat,
+        "lon": lon,
+        "api_key": API_Key,
+        "units": unit,
+    }
+
+    try: 
+        response = requests.get (url, parameters, timeout=5 )
+        response.raise_for_status ()
+
+        return response.json()
+
+    except requests.exceptions.HTTPError as http_err:
+        print(f"https error occured {http_err}")
+    except Exception as err:
+        print(f"error occured {err}")
+
+
+API_Key = api_key
+latitude = 33.4
+longitude = -94.04
+
+weather_json = get_info(API_Key, latitude, longitude)
+
+if weather_json:
+    current_temp = weather_json["current"]["temp"]
+    description = weather_json["current"]["weather"][0]["description"]
+    
+    print(f"Current Temperature: {current_temp}°C")
+    print(f"Conditions: {description.title()}")
+
+    
+
+    
